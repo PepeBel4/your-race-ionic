ecee527072492e26a9c3763f7648c26a9fe20686
Pascal Peeters <pascal.peeters@entrust-it.be>
2017-10-12 00:59:36 +0200

    ElementRef

--- src/pages/finish/finish.ts
+++ src/pages/finish/finish.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { CompetitorService } from '../../services/competitor.service';
