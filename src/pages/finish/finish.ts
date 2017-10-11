086d0456685e1b52f2fd6c2012122e88bccf6d56
Pascal Peeters <pascal.peeters@entrust-it.be>
2017-10-12 00:54:11 +0200

    gps location test

--- src/pages/finish/finish.ts
+++ src/pages/finish/finish.ts
@@ -5,6 +5,10 @@ import { CompetitorService } from '../../services/competitor.service';
 import { Competitor } from '../../services/competitor';
 import { FinishService } from '../../services/finish.service';
 
+import { Geolocation ,GeolocationOptions ,Geoposition ,PositionError } from '@ionic-native/geolocation';
+
+declare var google;
+
 @Component({
   selector: 'page-finish',
   templateUrl: 'finish.html'
@@ -15,10 +19,17 @@ export class FinishPage {
   competitors: Array<Competitor> = [];
   finishlist: Array<any> = [];
 
+  options : GeolocationOptions;
+  currentPos : Geoposition;
+
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
   constructor(
   	public navCtrl: NavController,
     public competitorService: CompetitorService,
-  	public finishService: FinishService) {
+  	public finishService: FinishService,
+    private geolocation: Geolocation) {
   }
 
   data: any;
@@ -27,6 +38,61 @@ export class FinishPage {
   ngOnInit() {
     this.getCompetitors();
   }
+
+  getUserPosition(){
+    this.options = {
+    enableHighAccuracy : false
+    };
+    this.geolocation.getCurrentPosition(this.options).then((pos : Geoposition) => {
+
+        this.currentPos = pos;     
+
+        console.log(pos);
+        this.addMap(pos.coords.latitude,pos.coords.longitude);
+
+    },(err : PositionError)=>{
+        console.log("error : " + err.message);
+    ;
+    })
+}
+
+ionViewDidEnter() {
+  this.getUserPosition();
+}   
+
+addMap(lat,long) {
+
+    let latLng = new google.maps.LatLng(lat, long);
+
+    let mapOptions = {
+    center: latLng,
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    this.addMarker();
+
+}
+
+addMarker(){
+
+    let marker = new google.maps.Marker({
+    map: this.map,
+    animation: google.maps.Animation.DROP,
+    position: this.map.getCenter()
+    });
+
+    let content = "<p>This is your current position !</p>";          
+    let infoWindow = new google.maps.InfoWindow({
+    content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', () => {
+    infoWindow.open(this.map, marker);
+    });
+
+}
 
   doClick(input,competitor) {
   	this.finishlist.push({id: competitor.id, timestamp: new Date().toISOString()});
